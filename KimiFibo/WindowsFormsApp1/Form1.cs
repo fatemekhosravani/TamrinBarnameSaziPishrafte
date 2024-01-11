67415e97b4c8486fa9410ec73bab69edce116050
fatemekhosravani <kimiikhosravanii@gmail.com>
2024-01-11 19:53:24 +0330

    ss

---
+++ KimiFibo/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string num = textBox1.Text;
+                int n = int.Parse(num);
+                int x;
+                n = n - 1;
+                if (n < 1)
+                {
+                    MessageBox.Show("Write Number greter Than Ziro");
+                    return;
+                }
+                x = fib(n);
+                MessageBox.Show("Member Of  " + (n + 1) + " Is:" + x.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private static int fib(int n)
+        {
+            if (n <= 0)
+                return n;
+            if (n == 1)
+                return n;
+            else
+                return fib(n - 1) + fib(n - 2);
+        }
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# TamrinBarnameSaziPishrafte
- 
+# Projcet_BarnameSaziPishrafte
+ Student : Fatemeh Khosravani
+ student ID : 140022028036
+ lecture : Dr Zare
+
